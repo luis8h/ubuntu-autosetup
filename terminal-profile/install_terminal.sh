128a15c8eadce654e365ce9c7851f85d980f3c47
Luis <vzbls@t-online.de>
2023-10-06 23:20:28 +0200

    changed installing of terminal

--- terminal-profile/install_terminal.sh
+++ terminal-profile/install_terminal.sh
@@ -8,10 +8,12 @@
 
 # Install ZSH
 sudo apt install -y git-core zsh curl
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
 
 # Set ZSH as the default shell for the current user
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
 
 echo "end of script"
 
