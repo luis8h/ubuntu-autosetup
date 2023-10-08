931963d4cb74ba86eff1eda325b3345cf93f0e43
Luis <vzbls@t-online.de>
2023-10-08 11:00:49 +0200

    added packer :so command to script

--- parts/nvim.sh
+++ parts/nvim.sh
@@ -7,3 +7,7 @@ snap install nvim --classic
 #installing packer 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+cp -r $DIR/../lib/nvim ~/.config/
+
+nvim -es -u ${HOME}/.config/nvim/lua/theprimeagen/packer.lua -c "so" -c "qa"
