1ad21cbf679fcc8fd6812835350273a12ea61f8e
Luis <vzbls@t-online.de>
2023-10-08 13:02:37 +0200

    finished nvim setup

--- parts/nvim.sh
+++ parts/nvim.sh
@@ -8,7 +8,17 @@ snap install nvim --classic
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+#install primeagen settings
 cp -r $DIR/../lib/nvim ~/.config/
 
+#running :so in packer file and :PackerSync
 nvim --headless -c 'so ~/.config/nvim/lua/theprimeagen/packer.lua' -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-#nvim -es -u ${HOME}/.config/nvim/lua/theprimeagen/packer.lua -c "so" -c "PackerSync" -c "qa"
+
+#install xclip for global copy/paste and ripgrep for searching
+sudo apt-get install xclip
+sudo apt-get install ripgrep
+
+#add alias to the .zshrc config file
+echo 'alias vim=nvim' >> ~/.zshrc
+
+
