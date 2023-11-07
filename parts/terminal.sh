2822facb8a22587c28943f97910e4c33ea704b91
Luis <vzbls@t-online.de>
2023-11-07 20:40:53 +0100

    added shortcut for autosuggestion in terminal

--- parts/terminal.sh
+++ parts/terminal.sh
@@ -5,3 +5,6 @@ DIR="$(dirname "$0")"
 ./$DIR/../lib/terminal-profile/install_terminal.sh
 ./$DIR/../lib/terminal-profile/install_profile.sh
 
+# change autosuggestion key to ctrl + l
+echo "bindkey '^l' autosuggest-accept" >> ~/.zshrc
+
