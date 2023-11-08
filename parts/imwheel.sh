4e06d618f32e4c04049ef9ec490b697d3cae5ba0
Luis <vzbls@t-online.de>
2023-11-08 15:47:50 +0100

    added imwheel installation and service initialization for better mouse wheel scrolling in chrome, terminal etc.

---
+++ parts/imwheel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$(dirname "$0")"
+
+sudo apt install imwheel -y
+
+mkdir -p ~/.config/systemd/
+mkdir -p ~/.config/systemd/user/
+
+cp $DIR/../lib/imwheel/imwheel.service ~/.config/systemd/user/imwheel.service
+
+systemctl --user daemon-reload imwheel
+systemctl --user start imwheel
+systemctl --user enable imwheel
--- start-setup.sh
+++ start-setup.sh
@@ -7,6 +7,7 @@ DIR="$(dirname "$0")"
 
 ./$DIR/parts/general.sh
 ./$DIR/parts/terminal.sh
+./$DIR/parts/imwheel.sh
 ./$DIR/parts/nvim.sh
 ./$DIR/parts/tmux.sh
 ./$DIR/parts/regolith.sh
