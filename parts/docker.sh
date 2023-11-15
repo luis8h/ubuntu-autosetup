3de9087c18903a70c94f734429336b732fc75866
luis8h <vzbls@t-online.de>
2023-11-15 06:28:48 +0100

    made docker usable without root

--- parts/docker.sh
+++ parts/docker.sh
@@ -19,3 +19,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# make useable without root
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
