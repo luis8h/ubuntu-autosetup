5a95e734601f9d91aa492f159a20c58c529d81fe
luis8h <vzbls@t-online.de>
2023-12-07 16:46:18 +0100

    fixed freefilesync installation

--- parts/install-programs.sh
+++ parts/install-programs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # free file sync
-wget https://freefilesync.org/download/FreeFileSync_11.28_Linux.tar.gz
-tar -xvf FreeFileSync_11.28_Linux.tar.gz
-sudo ./FreeFileSync_11.28_Install.run
+wget https://freefilesync.org/download/FreeFileSync_13.2_Linux.tar.gz
+sudo tar -zxvf FreeFileSync_13.2_Linux.tar.gz
+sudo ./FreeFileSync_13.2_Install.run
 
+# remove created files
+rm *deb
+rm *gz
