d6d9940d299f283a6c78cba5a6ad19a0297b139f
luis8h <vzbls@t-online.de>
2023-12-10 10:23:50 +0100

    added postman installation

--- parts/install-programs.sh
+++ parts/install-programs.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # google chrome
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # free file sync
 wget https://freefilesync.org/download/FreeFileSync_13.2_Linux.tar.gz
 sudo tar -zxvf FreeFileSync_13.2_Linux.tar.gz
 sudo ./FreeFileSync_13.2_Install.run
+
+# postman
+sudo snap install postman
 
 # remove created files
 rm *deb
