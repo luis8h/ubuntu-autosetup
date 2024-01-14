3cc4a626dc07c68921677ec83a7c48f363425162
luis8h <vzbls@t-online.de>
2024-01-14 08:47:54 +0100

    added liquibase installation

--- parts/install-programs.sh
+++ parts/install-programs.sh
@@ -12,6 +12,14 @@ sudo ./FreeFileSync_13.2_Install.run
 # postman
 sudo snap install postman
 
+# liquibase
+wget -O- https://repo.liquibase.com/liquibase.asc | gpg --dearmor > liquibase-keyring.gpg && \
+cat liquibase-keyring.gpg | sudo tee /usr/share/keyrings/liquibase-keyring.gpg > /dev/null && \
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/liquibase-keyring.gpg] https://repo.liquibase.com stable main' | sudo tee /etc/apt/sources.list.d/liquibase.list
+
+sudo apt-get update
+sudo apt-get install liquibase
+
 # remove created files
 rm *deb
 rm *gz
