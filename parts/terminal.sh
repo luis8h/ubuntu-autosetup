6cbff140bb6d14d81c34fd7df8e47eebb35765a3
luis8h <vzbls@t-online.de>
2024-02-12 19:32:20 +0100

    added zshell vi mode plugin

--- parts/terminal.sh
+++ parts/terminal.sh
@@ -8,3 +8,7 @@ DIR="$(dirname "$0")"
 # change autosuggestion key to ctrl + l
 echo "bindkey '^n' autosuggest-accept" >> ~/.zshrc
 
+# install vim plugin for zshell
+git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.zsh-vi-mode
+echo "source $HOME/.zsh-vi-mode/zsh-vi-mode.plugin.zsh" >> ~/.zshrc
+
