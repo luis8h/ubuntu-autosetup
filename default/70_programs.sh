b94e4fbb6989ffce45e24af677505e16fe7b33ec
Luis Schmidmeister <vzbls@t-online.de>
2024-02-29 21:17:04 +0100

    added java and liquibase

--- default/70_programs.sh
+++ default/70_programs.sh
@@ -17,6 +17,17 @@ wget https://freefilesync.org/download/FreeFileSync_13.2_Linux.tar.gz
 sudo tar -zxvf FreeFileSync_13.2_Linux.tar.gz
 sudo ./FreeFileSync_13.2_Install.run
 
+# java
+sudo apt install default-jdk
+sudo apt install default-jre
+
+# liquibase
+wget -O- https://repo.liquibase.com/liquibase.asc | gpg --dearmor > liquibase-keyring.gpg && \
+cat liquibase-keyring.gpg | sudo tee /usr/share/keyrings/liquibase-keyring.gpg > /dev/null && \
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/liquibase-keyring.gpg] https://repo.liquibase.com stable main' | sudo tee /etc/apt/sources.list.d/liquibase.list
+sudo apt-get update
+sudo apt-get install liquibase
+
 # postman
 sudo snap install postman
 
