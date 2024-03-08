7037d0827f948a50bf2307a599153dc86a22005c
Luis Schmidmeister <vzbls@t-online.de>
2024-03-08 16:17:15 +0100

    added pika backup installation

---
+++ additional/flatpak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install flatpak
+
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt install flatpak
+
+sudo apt install gnome-software-plugin-flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
---
+++ additional/pika-backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./flatpak.sh
+
+flatpak install flathub org.gnome.World.PikaBackup
