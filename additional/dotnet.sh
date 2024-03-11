da5f5400a56376cfb4c2d636de2e8d8121f00ca0
Luis Schmidmeister <vzbls@t-online.de>
2024-03-11 16:25:12 +0100

    added dotnet version 8

--- additional/dotnet.sh
+++ additional/dotnet.sh
@@ -7,3 +7,6 @@ sudo apt-get update && \
   sudo apt-get install -y aspnetcore-runtime-7.0
 
 dotnet tool install --global dotnet-ef
+
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt-get install -y aspnetcore-runtime-8.0
