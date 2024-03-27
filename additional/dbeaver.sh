240d53ed54ae8049da1a79f2f486cbb26730d5d8
luis8h <vzbls@t-online.de>
2024-03-27 15:14:18 +0100

    added dbeaver installation

---
+++ additional/dbeaver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -fsSL https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/dbeaver.gpg
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+
+sudo apt update && sudo apt install dbeaver-ce
