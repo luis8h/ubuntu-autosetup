e3ef25509c4abbfac4d7bf9b1fe58247dab79f71
Luis Schmidmeister <vzbls@t-online.de>
2024-04-05 16:14:50 +0200

    added vmbox file removing

--- additional/vmbox.sh
+++ additional/vmbox.sh
@@ -9,3 +9,5 @@ sudo apt install virtualbox-7.0
 
 wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+
+rm Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
