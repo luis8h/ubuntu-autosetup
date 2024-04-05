a5c2bcdf7cfac44f9123d718d0af28cb31427bc7
Luis Schmidmeister <vzbls@t-online.de>
2024-04-05 16:12:52 +0200

    added vmbox installation

---
+++ additional/steam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo add-apt-repository multiverse
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install steam -y
---
+++ additional/vmbox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install software-properties-common -y
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+
+sudo apt update -y
+sudo apt install virtualbox-7.0
+
+wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
