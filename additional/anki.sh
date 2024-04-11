6a0b377b24b264e8053c58d197721544c8679a5d
Luis Schmidmeister <vzbls@t-online.de>
2024-04-11 17:42:26 +0200

    improved anki installation

--- additional/anki.sh
+++ additional/anki.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+VERSION="24.04"
+
 sudo apt install libxcb-xinerama0 libxcb-cursor0 libnss3 -y
 sudo apt install zstd -y
-curl https://github.com/ankitects/anki/releases/download/24.04/anki-24.04-linux-qt6.tar.zst
 
-tar xaf ./anki-24.04-linux-qt6.tar.zst
-cd anki-2XXX-linux-qt6
+wget "https://github.com/ankitects/anki/releases/download/${VERSION}/anki-${VERSION}-linux-qt6.tar.zst"
+
+tar xaf ./anki-${VERSION}-linux-qt6.tar.zst
+cd anki-${VERSION}-linux-qt6
 sudo ./install.sh
+
+cd ..
+rm -r ./anki-${VERSION}-linux-qt6/
+rm ./anki-${VERSION}-linux-qt6.tar.zst
