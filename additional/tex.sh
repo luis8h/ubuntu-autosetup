79881b8cef94df421fb7701fe0c559ea6ce1684f
Luis Schmidmeister <vzbls@t-online.de>
2024-04-26 22:26:35 +0200

    added notes to tex installation

--- additional/tex.sh
+++ additional/tex.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# important:
+# if the installation gets stuck and writes something like: this may take some time ...
+# just press and hold enter until it continues
 sudo apt-get install texlive-full
