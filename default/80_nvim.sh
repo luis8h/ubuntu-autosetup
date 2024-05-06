bdb7f6df52390b0bf21dc9d21eb4e5030b1322da
Luis Schmidmeister <vzbls@t-online.de>
2024-05-06 21:01:10 +0200

    adjusted nvim installation to neovim

--- default/80_nvim.sh
+++ default/80_nvim.sh
@@ -9,14 +9,14 @@ sudo apt-get install -y postgresql-client
 bob use stable
 
 #installing packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+#  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 #install primeagen settings
 # cp -r $DIR/../../lib/nvim ~/.config/
 
 #running :so in packer file and :PackerSync
-nvim --headless -c 'so ~/.config/nvim/lua/theprimeagen/packer.lua' -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+# nvim --headless -c 'so ~/.config/nvim/lua/theprimeagen/packer.lua' -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 #install xclip for global copy/paste and ripgrep for searching
 sudo apt-get install xclip
