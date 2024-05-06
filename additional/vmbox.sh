17661695d07d308bc2f2dd18b6cfa8e7006a21b3
Luis Schmidmeister <vzbls@t-online.de>
2024-05-06 21:20:45 +0200

    change vmbox installation

--- additional/vmbox.sh
+++ additional/vmbox.sh
@@ -5,9 +5,12 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 
 sudo apt update -y
-sudo apt install virtualbox-7.0
+# sudo apt install virtualbox-7.0
+sudo apt install virtualbox-qt -y
 
 wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
 
 rm Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+
+
