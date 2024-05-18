5040b36e1aad7e60a276d51e88a08d8222aaac72
Luis Schmidmeister <vzbls@t-online.de>
2024-05-18 09:41:28 +0200

    added homebrew installation

--- default/11_package_managers.sh
+++ default/11_package_managers.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/luis8h/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential -y
+
+
 # sdkman
 
 sudo apt-get install unzip -y
@@ -19,3 +26,5 @@ curl https://sh.rustup.rs -sSf | sh
 
 ## if it fails here, just close terminal and try again because cargo was installed just before
 cargo install --git https://github.com/MordechaiHadad/bob.git
+
+
