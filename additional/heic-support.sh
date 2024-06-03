582bd7681af692361fb02f3bfd95dcf6d7aefe5e
Luis Schmidmeister <vzbls@t-online.de>
2024-06-03 22:05:59 +0200

    added findimagedupes

--- additional/heic-support.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# additionaly install eye of gnome in ubuntu software store
-
-sudo apt install heif-thumbnailer -y
-sudo apt install heif-gdk-pixbuf -y
---
+++ additional/image-sorting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# image duplicates
+sudo apt install findimagedupes -y
+
+# heic support
+# additionaly install eye of gnome in ubuntu software store
+sudo apt install heif-thumbnailer -y
+sudo apt install heif-gdk-pixbuf -y
